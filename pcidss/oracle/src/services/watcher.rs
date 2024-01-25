a44ae0a155505a79108672aff8c49254c23536fb
dastansam <dastan.samatov.33@gmail.com>
2024-01-25 22:19:04 +0100

    Fix tests

--- pcidss/oracle/src/services/watcher.rs
+++ pcidss/oracle/src/services/watcher.rs
@@ -104,45 +104,7 @@ impl WatcherService {
 					// spread struct properties into variables
 					let InitiateTransfer { from, to, amount } = decoded_event;
 
-					let (from_hex, to_hex): (String, String) = (
-						from.0.iter().map(|b| format!("{:02x}", b)).collect(),
-						from.0.iter().map(|b| format!("{:02x}", b)).collect(),
-					);
-
-					let (from_bank_account, to_bank_account) = futures::join!(
-						self.processor
-							.bank_account_controller
-							.find_by_account_id(&from_hex.as_str()),
-						self.processor.bank_account_controller.find_by_account_id(&to_hex.as_str())
-					);
-
-					let from_bank_account =
-						from_bank_account?.ok_or("From bank account not found")?;
-					let to_bank_account = to_bank_account?.ok_or("To bank account not found")?;
-
-					log::debug!(
-						"Processing transaction from: {}, to: {}, amount: {}, event_id: {}",
-						from_hex,
-						to_hex,
-						amount,
-						&event_id
-					);
-
-					// compose ISO8583 message
-					let mut iso_msg_raw = self
-						.compose_iso_msg(
-							&from_bank_account,
-							Some(&to_bank_account),
-							None,
-							amount,
-							&event_id,
-						)
-						.map_err(|_| "Could not compose ISO8583 message")?;
-
-					let (_, iso_msg) = self.processor.process(&mut iso_msg_raw).await?;
-
-					// submit finality
-					self.submit_finality(from, to, amount, iso_msg, &event_id).await?;
+					Self::process_transfer(&self, from, to, amount, &event_id).await?
 				},
 				x if x.contains("InitiateRevert") => {
 					let decoded_event =
@@ -150,101 +112,11 @@ impl WatcherService {
 
 					let InitiateRevert { who, hash } = decoded_event;
 
-					let (who_hex, hash_hex): (String, String) = (
-						who.0.iter().map(|b| format!("{:02x}", b)).collect(),
-						hash.0.iter().map(|b| format!("{:02x}", b)).collect(),
-					);
-
-					log::debug!("Reverting transaction from: {}, hash: {}", who_hex, hash_hex);
-
-					let (from_bank_account, maybe_transaction) = futures::join!(
-						self.processor
-							.bank_account_controller
-							.find_by_account_id(&who_hex.as_str()),
-						self.processor.transaction_controller.find_by_hash(&hash_hex.as_str())
-					);
-
-					let from_bank_account =
-						from_bank_account?.ok_or("From bank account not found")?;
-					// simply try to unwrap the transaction, if it's not found, then it's an error
-					// we do this to avoid doing unnecessary ISO8583 processing and as a naive DDOS
-					// protection
-					let _ = maybe_transaction?.ok_or("Transaction not found")?;
-
-					let mut iso_msg_raw = self
-						.compose_iso_msg(&from_bank_account, None, Some(&hash_hex), 0, &event_id)
-						.map_err(|_| "Could not compose ISO8583 message")?;
-
-					let (_, iso_msg) = self.processor.process(&mut iso_msg_raw).await?;
-
-					self.submit_finality(
-						who,
-						AccountId32(
-							PALLET_ACCOUNT.as_bytes().try_into().expect("valid account;qed"),
-						),
-						0,
-						iso_msg,
-						&event_id,
-					)
-					.await?;
+					Self::process_revert(&self, who, hash, &event_id).await?
 				},
 				_ => (),
 			}
 		}
-
-		Ok(())
-	}
-
-	/// Submit a processed ISO8583 message on-chain
-	///
-	/// This is called after an ISO-8583 message has been processed by the ISO8583 message
-	/// processor, both from the watcher service or the RPC server
-	pub(crate) async fn submit_finality(
-		&self,
-		from: AccountId32,
-		to: AccountId32,
-		amount: u128,
-		iso_msg: IsoMsg,
-		event_id: &str,
-	) -> anyhow::Result<(), Box<dyn std::error::Error>> {
-		let private_data =
-			iso_msg.bmp_child_value(126).map_err(|_| "Could not get private data")?;
-
-		let (tx_hash, _) = private_data.split_at(64);
-
-		let response_code =
-			iso_msg.bmp_child_value(39).map_err(|_| "Could not get response code")?;
-
-		let status = match &response_code[..] {
-			"00" => ISO8583Status::Approved,
-			"05" => ISO8583Status::Failed(ISO8583FailureReason::DoNotHonor),
-			"12" => ISO8583Status::Failed(ISO8583FailureReason::InvalidTransaction),
-			"14" => ISO8583Status::Failed(ISO8583FailureReason::InvalidCardNumber),
-			"51" => ISO8583Status::Failed(ISO8583FailureReason::InsufficientFunds),
-			"54" => ISO8583Status::Failed(ISO8583FailureReason::ExpiredCard),
-			_ => ISO8583Status::Failed(ISO8583FailureReason::Other),
-		};
-
-		let finalised_transacton = FinalisedTransaction {
-			hash: H256::from_str(tx_hash.strip_prefix("0x").unwrap_or_default())
-				.expect("valid hash; qed"),
-			event_id: BoundedVec::<u8>(event_id.as_bytes().to_vec()),
-			from,
-			to,
-			amount,
-			status,
-		};
-
-		log::debug!("Submitting finality: {:?}", finalised_transacton);
-
-		let finalised_transaction_tx =
-			iso_8583_chain::tx().iso8583().submit_finality(finalised_transacton);
-
-		// don't wait for the transaction to be included in a block, submit and forget
-		self.client
-			.tx()
-			.sign_and_submit_default(&finalised_transaction_tx, &self.keypair)
-			.await?;
 
 		Ok(())
 	}
@@ -301,5 +173,143 @@ impl WatcherService {
 
 		let iso_msg_raw = msg.assemble()?;
 		Ok(iso_msg_raw)
+	}
+}
+
+// Separate utility functions into a separate module
+impl WatcherService {
+	/// Process a transfer event
+	pub(crate) async fn process_transfer(
+		&self,
+		from: AccountId32,
+		to: AccountId32,
+		amount: u128,
+		event_id: &str,
+	) -> anyhow::Result<(), Box<dyn std::error::Error>> {
+		let (from_hex, to_hex): (String, String) = (
+			from.0.iter().map(|b| format!("{:02x}", b)).collect(),
+			from.0.iter().map(|b| format!("{:02x}", b)).collect(),
+		);
+
+		let (from_bank_account, to_bank_account) = futures::join!(
+			self.processor.bank_account_controller.find_by_account_id(&from_hex.as_str()),
+			self.processor.bank_account_controller.find_by_account_id(&to_hex.as_str())
+		);
+
+		let from_bank_account = from_bank_account?.ok_or("From bank account not found")?;
+		let to_bank_account = to_bank_account?.ok_or("To bank account not found")?;
+
+		log::debug!(
+			"Processing transaction from: {}, to: {}, amount: {}, event_id: {}",
+			from_hex,
+			to_hex,
+			amount,
+			&event_id
+		);
+
+		// compose ISO8583 message
+		let mut iso_msg_raw = self
+			.compose_iso_msg(&from_bank_account, Some(&to_bank_account), None, amount, &event_id)
+			.map_err(|_| "Could not compose ISO8583 message")?;
+
+		let (_, iso_msg) = self.processor.process(&mut iso_msg_raw).await?;
+
+		// submit finality
+		self.submit_finality(from, to, amount, iso_msg, &event_id).await
+	}
+
+	/// Process a revert event
+	pub(crate) async fn process_revert(
+		&self,
+		from: AccountId32,
+		hash: H256,
+		event_id: &str,
+	) -> anyhow::Result<(), Box<dyn std::error::Error>> {
+		let (who_hex, hash_hex): (String, String) = (
+			from.0.iter().map(|b| format!("{:02x}", b)).collect(),
+			hash.0.iter().map(|b| format!("{:02x}", b)).collect(),
+		);
+
+		log::debug!("Reverting transaction from: {}, hash: {}", who_hex, hash_hex);
+
+		let (from_bank_account, maybe_transaction) = futures::join!(
+			self.processor.bank_account_controller.find_by_account_id(&who_hex.as_str()),
+			self.processor.transaction_controller.find_by_hash(&hash_hex.as_str())
+		);
+
+		let from_bank_account = from_bank_account?.ok_or("From bank account not found")?;
+		// simply try to unwrap the transaction, if it's not found, then it's an error
+		// we do this to avoid doing unnecessary ISO8583 processing and as a naive DDOS
+		// protection
+		let _ = maybe_transaction?.ok_or("Transaction not found")?;
+
+		let mut iso_msg_raw = self
+			.compose_iso_msg(&from_bank_account, None, Some(&hash_hex), 0, &event_id)
+			.map_err(|_| "Could not compose ISO8583 message")?;
+
+		let (_, iso_msg) = self.processor.process(&mut iso_msg_raw).await?;
+
+		self.submit_finality(
+			from,
+			AccountId32(PALLET_ACCOUNT.as_bytes().try_into().expect("valid account;qed")),
+			0,
+			iso_msg,
+			&event_id,
+		)
+		.await
+	}
+
+	/// Submit a processed ISO8583 message on-chain
+	///
+	/// This is called after an ISO-8583 message has been processed by the ISO8583 message
+	/// processor, both from the watcher service or the RPC server
+	pub(crate) async fn submit_finality(
+		&self,
+		from: AccountId32,
+		to: AccountId32,
+		amount: u128,
+		iso_msg: IsoMsg,
+		event_id: &str,
+	) -> anyhow::Result<(), Box<dyn std::error::Error>> {
+		let private_data =
+			iso_msg.bmp_child_value(126).map_err(|_| "Could not get private data")?;
+
+		let (tx_hash, _) = private_data.split_at(64);
+
+		let response_code =
+			iso_msg.bmp_child_value(39).map_err(|_| "Could not get response code")?;
+
+		let status = match &response_code[..] {
+			"00" => ISO8583Status::Approved,
+			"05" => ISO8583Status::Failed(ISO8583FailureReason::DoNotHonor),
+			"12" => ISO8583Status::Failed(ISO8583FailureReason::InvalidTransaction),
+			"14" => ISO8583Status::Failed(ISO8583FailureReason::InvalidCardNumber),
+			"51" => ISO8583Status::Failed(ISO8583FailureReason::InsufficientFunds),
+			"54" => ISO8583Status::Failed(ISO8583FailureReason::ExpiredCard),
+			_ => ISO8583Status::Failed(ISO8583FailureReason::Other),
+		};
+
+		let finalised_transacton = FinalisedTransaction {
+			hash: H256::from_str(tx_hash.strip_prefix("0x").unwrap_or_default())
+				.expect("valid hash; qed"),
+			event_id: BoundedVec::<u8>(event_id.as_bytes().to_vec()),
+			from,
+			to,
+			amount,
+			status,
+		};
+
+		log::debug!("Submitting finality: {:?}", finalised_transacton);
+
+		let finalised_transaction_tx =
+			iso_8583_chain::tx().iso8583().submit_finality(finalised_transacton);
+
+		// don't wait for the transaction to be included in a block, submit and forget
+		self.client
+			.tx()
+			.sign_and_submit_default(&finalised_transaction_tx, &self.keypair)
+			.await?;
+
+		Ok(())
 	}
 }
--- pcidss/oracle/src/tests/watcher.rs
+++ pcidss/oracle/src/tests/watcher.rs
@@ -2,16 +2,18 @@
 
 use std::{str::FromStr, sync::Arc};
 
-use subxt::metadata::types::Metadata;
 use subxt_signer::SecretUri;
 
-use crate::{services::watcher::WatcherService, tests::mock::*};
+use crate::{
+	services::watcher::WatcherService,
+	tests::{mock::*, prelude::get_bank_account_by_card_number},
+};
 
 /// Test uri
 const SEED: &str = "//Alice";
 
 #[tokio::test]
-async fn test_process_transfer() {
+async fn test_compose_iso_msg() {
 	let mock_processor = Arc::new(MockProcessorImpl::new(None).await);
 	let watcher = WatcherService::new(
 		SecretUri::from_str(SEED).unwrap(),
@@ -21,16 +23,18 @@ async fn test_process_transfer() {
 	.await
 	.unwrap();
 
-	watcher.process_event(1, EventDetails{
-        phase: Phase::ApplyExtrinsic(0),
-        index: 0,
-        all_bytes: vec![],
-        start_idx: 0,
-        event_stard_idx: 0,
-        event_fields_start_idx: 0,
-        event_fields_end_idx: 0,
-        end_idx: 0,
-        metadata: Metadata::default(),
+	let spec = iso8583_rs::iso8583::iso_spec::spec("");
 
-    }
+	let from = get_bank_account_by_card_number(&mock_processor, "1111222233334444").await;
+	let to = get_bank_account_by_card_number(&mock_processor, "5555666677778888").await;
+
+	let mut iso_msg = watcher.compose_iso_msg(&from, Some(&to), None, 1234, "123-1").unwrap();
+
+	let iso_msg_parsed = spec.parse(&mut iso_msg).unwrap();
+
+	assert_eq!(iso_msg_parsed.get_field_value(&"message_type".to_string()).unwrap(), "0200");
+	assert_eq!(iso_msg_parsed.bmp_child_value(2).unwrap(), "5555666677778888");
+	assert_eq!(iso_msg_parsed.bmp_child_value(3).unwrap(), "000000");
+	assert_eq!(iso_msg_parsed.bmp_child_value(4).unwrap(), "00000000001234");
+	assert_eq!(iso_msg_parsed.bmp_child_value(7).unwrap(), "123-1");
 }
